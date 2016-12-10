e2b4418b396c1f42434d108f07870990c8ab00a0
Kristopher Ruzic <krruzic@gmail.com>
2016-12-10 02:10:14 -0700

    implement read function more fully

--- src/wav.c
+++ src/wav.c
@@ -4,8 +4,19 @@
 #include <stdlib.h>
 #include "wav.h"
 
+void write_little_endian(unsigned int word, int num_bytes, FILE *wav_file) {
+  unsigned buf;
+  while(num_bytes > 0) {   
+    buf = word & 0xff;
+    fwrite(&buf, 1,1, wav_file);
+    num_bytes--;
+  word >>= 8;
+  }
+}
+
+
 float *readWav(char *filename, float *signal, WAV *header) {
-  read = fread(header->riff, sizeof(header->riff), 1, file);
+  read = fread(header->chunkID, sizeof(header->chunkID), 1, file);
   read = fread(buffer4, sizeof(buffer4), 1, file);
   // convert little endian to big endian 4 byte int
   header->overall_size  = buffer4[0] |
@@ -14,67 +25,101 @@ float *readWav(char *filename, float *signal, WAV *header) {
             (buffer4[3]<<24);
 
 
-  read = fread(header->wave, sizeof(header->wave), 1, file);
-  read = fread(header->fmt_chunk_marker, sizeof(header->fmt_chunk_marker), 1, file);
+  read = fread(header->format, sizeof(header->format), 1, file);
+  read = fread(header->subChunk1ID, sizeof(header->subChunk1ID), 1, file);
 
   read = fread(buffer4, sizeof(buffer4), 1, file);
 
   // convert little endian to big endian 4 byte integer
-  header->length_of_fmt = buffer4[0] |
+  header->subChunk1Size = buffer4[0] |
               (buffer4[1] << 8) |
               (buffer4[2] << 16) |
               (buffer4[3] << 24);
 
   read = fread(buffer2, sizeof(buffer2), 1, file);
-  header->format_type = buffer2[0] | (buffer2[1] << 8);
-
-  // if not PCM, there's an extra 2 fields in the file before data
-  char format_name[10] = "";
-  if (header->format_type == 1)
-    strcpy(format_name,"PCM");
-  else if (header->format_type == 6)
-    strcpy(format_name, "A-law");
-  else if (header->format_type == 7)
-    strcpy(format_name, "Mu-law");
+  header->audioFormat = buffer2[0] | (buffer2[1] << 8);
 
   read = fread(buffer2, sizeof(buffer2), 1, file);
-  header->channels = buffer2[0] | (buffer2[1] << 8);
+  header->numChannels = buffer2[0] | (buffer2[1] << 8);
 
   read = fread(buffer4, sizeof(buffer4), 1, file);
-  header->sample_rate = buffer4[0] |
+  header->sampleRate = buffer4[0] |
             (buffer4[1] << 8) |
             (buffer4[2] << 16) |
             (buffer4[3] << 24);
 
 
   read = fread(buffer4, sizeof(buffer4), 1, file);
-  header->byterate  = buffer4[0] |
+  header->byteRate  = buffer4[0] |
             (buffer4[1] << 8) |
             (buffer4[2] << 16) |
             (buffer4[3] << 24);
 
   read = fread(buffer2, sizeof(buffer2), 1, file);
-  header->block_align = buffer2[0] |
+  header->blockAlign = buffer2[0] |
           (buffer2[1] << 8);
 
   read = fread(buffer2, sizeof(buffer2), 1, file);
-  header->bits_per_sample = buffer2[0] |
+  header->bitsPerSample = buffer2[0] |
           (buffer2[1] << 8);
 
-  read = fread(header->data_chunk_header, sizeof(header->data_chunk_header), 1, file);
+  read = fread(header->subChunk2ID, sizeof(header->subChunk2ID), 1, file);
 
   read = fread(buffer4, sizeof(buffer4), 1, file);
-  header->data_size = buffer4[0] |
+  header->subChunk2Size = buffer4[0] |
         (buffer4[1] << 8) |
         (buffer4[2] << 16) |
         (buffer4[3] << 24 );
+
+  int data_size = subChunk2Size/2;
+  short *data = new short[data_size];
+  
+  for (i = 0; i < data_size; i++) {
+    read = fread(buffer2, sizeof(buffer2), 1, file);
+    data[i] = buffer2[0] | (buffer2[1] << 8);
+  }
+  fclose(wav_file);
+
+
 }
 
-void writeWav(char *fileName, WAV *header) {
+void writeWav(char *fileName, WAV *header, short data[], int signalSize) {
   FILE* wav_file;
 
   wav_file = fopen(filename, "w");
   if (wav_file == NULL) {
     exit(1);
   }
+  
+  // only supports 1 channel
+  bytesPerSample = header->bitsPerSample / 8;
+  subChunk2Size = signalSize * (header->bitsPerSample / 8); 
+  chunkSize = 36 + subChunk2Size;
+ 
+  /* RIFF chunk */
+  fwrite("RIFF", 1, 4, wav_file);
+  write_little_endian(chunkSize);
+  fwrite("WAVE", 1, 4, wav_file);
+  
+  /* fmt chunk */
+  fwrite("fmt", 1, 4, wav_file);
+  write_little_endian(16, 4, wav_file);   /* SubChunk1Size is 16 */
+  write_little_endian(1, 2, wav_file);    /* PCM is format 1 */
+
+  /* data chunk */
+  write_little_endian(1, 2, wav_file); /* only support ONE channel */
+  write_little_endian(header->sampleRate, 4, wav_file);
+  write_little_endian(header->byteRate, 4, wav_file);
+  write_little_endian(signalSize * bytesPerSample, 2, wav_file);  /* block align */
+  write_little_endian(header->bitsPerSample, 2, wav_file);  /* bits/sample */
+
+  /* write data subchunk */
+  fwrite("data", 1, 4, wav_file);
+  // only support one channel
+  write_little_endian(bytesPerSample * signalSize, 4, wav_file);
+  for (i=0; i< signalSize; i++) {
+    write_little_endian((unsigned int)(data[i]),bytes_per_sample, wav_file);
+  }
+
+  fclose(wav_file);
 }
--- src/wav.h
+++ src/wav.h
@@ -1,17 +1,28 @@
-// WAVE file header format
-struct WAV_HEADER {
-    unsigned char riff[4];                      // RIFF string
-    unsigned int overall_size;                  // overall size of file in bytes
-    unsigned char wave[4];                      // WAVE string
-    unsigned char fmt_chunk_marker[4];          // fmt string with trailing null char
-    unsigned int length_of_fmt;                 // length of the format data
-    unsigned int format_type;                   // format type. 1-PCM, 3- IEEE float, 6 - 8bit A law, 7 - 8bit mu law
-    unsigned int channels;                      // no.of channels
-    unsigned int sample_rate;                   // sampling rate (blocks per second)
-    unsigned int byterate;                      // SampleRate * NumChannels * BitsPerSample/8
-    unsigned int block_align;                   // NumChannels * BitsPerSample/8
-    unsigned int bits_per_sample;               // bits per sample, 8- 8bits, 16- 16 bits etc
-    unsigned char data_chunk_header [4];        // DATA string or FLLR string
-    unsigned int data_size;                     // NumSamples * NumChannels * BitsPerSample/8 - size of the next chunk that will be read
-};
+#ifndef WAV_H
+#define WAV_H
 
+// WAVE file header format
+typedef struct WAV_HEADER {
+  // RIFF chunk descriptor
+  unsigned char chunkID[4];            // RIFF string
+  unsigned int overall_size;           // overall size of file in bytes
+  unsigned char format[4];             // WAVE string
+  
+  // fmt sub-chunk
+  unsigned char subChunk1ID[4];        // fmt string with trailing null char
+  unsigned int subChunk1Size;          // length of the format data
+  unsigned int audioFormat;            // 1-PCM, 3-IEEE, 6-A law, 7-mu law
+  unsigned int numChannels;            // no.of channels
+  unsigned int sampleRate;             // sampling rate (blocks per second)
+  unsigned int byteRate;               // rate * channels * BitsPerSample/8
+  unsigned int blockAlign;             // NumChannels * BitsPerSample/8
+  unsigned int bitsPerSample;          // 8- 8bits, 16- 16 bits etc
+  
+  // date sub-chunk
+  unsigned char subChunk2ID[4];        // DATA string or FLLR string
+  unsigned int subChunk2Size;          // : samples * channels * BitsPerSample/8
+  short* fileData;                     // a pointer to the array of data 
+} WAV;
+
+float *readWav(char *filename, float *signal, WAV *header);
+void writeWav(char *fileName, WAV *header);
